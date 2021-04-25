85ede9589a828ba85927b0f6784c203c61cc75ef
Claudio Iury <iurylemos4@gmail.com>
2021-04-25 12:00:38 -0300

    generate chave secret

--- main.go
+++ main.go
@@ -10,6 +10,16 @@ import (
 	"net/http"
 )
 
+// func init() {
+// 	hashKey := hex.EncodeToString(securecookie.GenerateRandomKey(16))
+
+// 	fmt.Println(hashKey)
+
+// 	blockKey := hex.EncodeToString(securecookie.GenerateRandomKey(16))
+
+// 	fmt.Println(blockKey)
+// }
+
 func main() {
 	config.LoadEnviroment()
 	cookies.ConfigureCookie()
